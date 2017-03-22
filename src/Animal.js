522d20f893dc050523e1cff00510b255ebccb64c
Ryan Cronin <Ryan@Ryans-MBP.home>
2017-03-21 20:39:24 -0400

    successfull error for animal(name)

--- src/Animal.js
+++ src/Animal.js
@@ -1,49 +1,52 @@
 (function() {
   'use strict';
   window.zoo = window.zoo || {};
-  /**
-   * Creates a blueprint for the class of Animal
-   * @type {Object}
-   */
+
   window.zoo.Animal = class Animal {
     /**
-     * creates data for individual Animals to be added to the Animal object
-     */
+    * Constructs a new Animal
+    * @param  {String} name       Name of the Animal
+    * @param  {Date} dateOfBirth  Date the animal was born
+    * @return {void}
+    */
     constructor(name, dateOfBirth){
+      if (typeof(name) === 'string'){
+        this.name = name;
+      } else {
+        throw TypeError('Please provide a string');
 
-      this.name = name;
+      }
+
       if (!(dateOfBirth instanceof Date)){
         dateOfBirth = new Date();
       }
+      
       this.dob = dateOfBirth;
     }
     /**
-     * Allows you to change the name of the Animal
-     */
+    * Allows you to change the name of the Animal
+    * @param {String} name   New name of Animal
+    * @return {Void}
+    */
     changeName(newName){
       this.name = newName;
     }
     /**
-     * Allows you to determine the age of the Animal
-     */
+    * Gets the current age of the Animal in whole years
+    * @return {Number} The age of the Animal
+    */
     age(){
-      let now = new Date();
-      let ageInYears = (now.getFullYear() - this.dob.getFullYear());
-      console.log(ageInYears);
-      return ageInYears;
+      return (new Date()).getFullYear() - this.dob.getFullYear();
     }
     /**
-     * converts Data within the Animal object to a string
-     */
+    * converts Data within the Animal object to a string
+    * @return {String}   name and dob of animal
+    */
     toString(){
       let result = this.name + ' was born  ' + this.dob.getFullYear();
       return result;
     }
 
-
-
   };
-
-
 
 }());
--- src/zoo.js
+++ src/zoo.js
@@ -1,25 +1,37 @@
 (function() {
   'use strict';
+  window.zoo = window.zoo || {};
   let Animal = window.zoo.Animal;
   let Dog = window.zoo.Dog;
   let Cat = window.zoo.Cat;
 
 
-  let zoe = new Dog('zoe', new Date('3/17/2008'), 'run');
-  console.log(zoe);
+
+  let zoe = new Dog('zoe', new Date('3/17/2008'));
+  // console.log(zoe);
   zoe.changeName('Mickey');
-  console.log(zoe.name);
-  console.log(Dog.toString());
+  // console.log(zoe.name);
+  // console.log(Dog.toString());
   zoe.age();
-  console.log(zoe.toString());
+  // console.log(zoe.toString());
 
 
-  let peaches = new Cat('peaches', new Date('3/17/2009'), 'climb');
-  console.log(peaches);
+  let peaches = new Cat('peaches', new Date('3/17/2009'));
+  // console.log(peaches);
   peaches.changeName('Kodi');
-  console.log(peaches.name);
-  console.log(Cat.toString());
+  // console.log(peaches.name);
+  // console.log(Cat.toString());
   peaches.age();
-  console.log(peaches.toString());
+  // console.log(peaches.toString());
 
-}());
+  // try ... catch
+  try {
+    let newVar = new Animal();
+  } catch(err) {
+    console.warn(err);
+  }
+
+
+
+
+})();
